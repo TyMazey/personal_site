a56de9dccc461b0d74914469e4a93deddb6f04ec
Ty Mazey <tymazey@gmail.com>
2019-01-14 15:20:55 -0700

    updates about page test

---
+++ test/features/user_can_visit_about_page_test.rb
@@ -0,0 +1,10 @@
+require './test/test_helper'
+
+class HomepageTest < CapybaraTestCase
+  def test_user_can_see_the_homepage
+    visit '/about'
+
+    assert page.has_content?("About Me!")
+    assert_equal 200, page.status_code
+  end
+end
